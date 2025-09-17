9f1e42b5d66fd7f55c5a433620844557aa1c4943
aramba0 <166862845+aramba0@users.noreply.github.com>
2025-09-17 17:21:30 +0300

    Create script.js

---
+++ calculator/script.js
@@ -0,0 +1,55 @@
+let button = document.querySelectorAll('.num');
+let input = document.getElementById('display');
+let realInputText = "";
+let stack = "";
+function addToDisplay(data, visualText){
+    console.log(realInputText[realInputText.length-1]);
+    if(data === "C"){
+        realInputText='';
+        input.innerText='';
+    }else if(isNaN(data)){ //NOT A NUMBER
+        if(input.innerText != '' || data == '('){
+            if(data != '='){
+                if(data === '('){
+                    stack += data;
+                    realInputText+=data;
+                    input.innerText+=visualText;
+                }else if(data === ')'){
+                    let opened=0;
+                    let closed=0;
+                    for(let i=0;i<stack.length;i++){
+                        if(stack[i] === '('){
+                            opened++;
+                        }else{
+                            closed++;
+                        }
+                    }
+                    if(opened < closed){
+                        return;
+                    }
+                    realInputText+=data;
+                    input.innerText+=visualText;
+                }
+                if(!isNaN(realInputText[realInputText.length-1])){
+                    
+                    realInputText+=data;
+                    input.innerText+=visualText;
+                }
+            }else{
+                input.innerText=eval(realInputText);
+                realInputText=input.innerText;
+            }
+        }
+    }else{
+        realInputText+=visualText;
+        input.innerText+=data;
+    }
+    
+
+}
+
+button.forEach(function(button){
+    button.addEventListener('click', function(e){
+        addToDisplay(e.target.getAttribute('data-value'),e.target.innerText);
+    });
+});
